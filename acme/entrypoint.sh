6c30caf9adfa326011c5e5f567be9e06fb81cfaf
Noocktall Klown S. <nklowns@gmail.com>
2025-11-09 17:02:15 -0300

    refactor: ajusta variáveis e estrutura do entrypoint.sh, atualiza configuração do Traefik e adiciona serviço acme-duckdns ao docker-compose

--- acme/entrypoint.sh
+++ acme/entrypoint.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 set -euo pipefail
 
-DOMAIN=${DOMAIN_DUCKDNS:-"drake-ayu.duckdns.org"}
-EMAIL=${ACME_EMAIL:-"your.email+acme@domain.com"}
-DuckDNS_Token=${DUCKDNS_TOKEN:-""}
+DUCKDNS_TOKEN=${DUCKDNS_TOKEN:-""}
+DOMAIN_DUCKDNS=${DOMAIN_DUCKDNS:-"drake-ayu.duckdns.org"}
+ACME_EMAIL=${ACME_EMAIL:-"your.email+acme@domain.com"}
 
 # Exporta a variável com o nome que o plugin espera
-export DuckDNS_Token
+export DUCKDNS_TOKEN
 
-if [[ -z "$DuckDNS_Token" ]]; then
+if [[ -z "$DUCKDNS_TOKEN" ]]; then
   echo "ERRO: DUCKDNS_TOKEN não definido"
   exit 1
 fi
 
-echo "[INFO] Registrando conta no ZeroSSL com email: $EMAIL"
-/root/.acme.sh/acme.sh --register-account -m "$EMAIL" --server zerossl
+echo "[INFO] Registrando conta no ZeroSSL com email: $ACME_EMAIL"
+/root/.acme.sh/acme.sh --register-account -m "$ACME_EMAIL" --server zerossl
 
-if [[ ! -f /certs/$DOMAIN.key ]]; then
+if [[ ! -f /certs/$DOMAIN_DUCKDNS.key ]]; then
   echo "[INFO] Primeira emissão"
-  echo "[INFO] Emitindo certificado para: $DOMAIN"
+  echo "[INFO] Emitindo certificado para: $DOMAIN_DUCKDNS"
   /root/.acme.sh/acme.sh --issue \
     --dns dns_duckdns \
-    -d "$DOMAIN" \
+    -d "$DOMAIN_DUCKDNS" \
     --server zerossl \
     --yes-I-know-dns-manual-mode-enough-go-ahead-please
 
   echo "[INFO] Instalando certificados em /certs"
-  /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN" \
-    --key-file /certs/$DOMAIN.key \
-    --fullchain-file /certs/$DOMAIN.crt \
-    --reloadcmd "echo [INFO] Certificado renovado para $DOMAIN"
+  /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN_DUCKDNS" \
+    --key-file /certs/$DOMAIN_DUCKDNS.key \
+    --fullchain-file /certs/$DOMAIN_DUCKDNS.crt \
+    --reloadcmd "echo [INFO] Certificado renovado para $DOMAIN_DUCKDNS"
 else
   echo "[INFO] Certificado já existe, iniciando modo daemon"
 fi
