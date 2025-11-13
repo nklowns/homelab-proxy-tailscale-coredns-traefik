fa47350ef5ea54fb2e27d8479f9975f4c92bb915
Noocktall Klown S. <nklowns@gmail.com>
2025-11-12 23:29:54 -0300

    Refatorar entrypoint.sh para melhorar a legibilidade e corrigir variáveis de ambiente; atualizar regras no traefik-dynamic.yml.tmpl para simplificação.

--- acme/entrypoint.sh
+++ acme/entrypoint.sh
@@ -1,32 +1,40 @@
 #!/bin/bash
 set -euo pipefail
 
-DUCKDNS_TOKEN=${DUCKDNS_TOKEN:-""}
+DuckDNS_Token=${DUCKDNS_TOKEN:-""}
 DOMAIN_DUCKDNS=${DOMAIN_DUCKDNS:-"drake-ayu.duckdns.org"}
 ACME_EMAIL=${ACME_EMAIL:-"your.email+acme@domain.com"}
 
 # Exporta a variável com o nome que o plugin espera
-export DUCKDNS_TOKEN
+export DuckDNS_Token
 
-if [[ -z "$DUCKDNS_TOKEN" ]]; then
-  echo "ERRO: DUCKDNS_TOKEN não definido"
+if [[ -z "$DuckDNS_Token" ]]; then
+  echo "ERRO: DuckDNS_Token não definido"
   exit 1
 fi
 
 echo "[INFO] Registrando conta no ZeroSSL com email: $ACME_EMAIL"
-/root/.acme.sh/acme.sh --register-account -m "$ACME_EMAIL" --server zerossl
+/root/.acme.sh/acme.sh \
+  --register-account \
+  -m "$ACME_EMAIL" \
+  --server zerossl
 
 if [[ ! -f "/certs/${DOMAIN_DUCKDNS}.key" ]]; then
   echo "[INFO] Primeira emissão"
   echo "[INFO] Emitindo certificado para: $DOMAIN_DUCKDNS"
-  /root/.acme.sh/acme.sh --issue \
+  /root/.acme.sh/acme.sh \
+    --issue \
     --dns dns_duckdns \
+    --dnssleep 120 \
     -d "$DOMAIN_DUCKDNS" \
-    --server zerossl \
-    --yes-I-know-dns-manual-mode-enough-go-ahead-please
+    -d "*.$DOMAIN_DUCKDNS" \
+    --server zerossl
 
   echo "[INFO] Instalando certificados em /certs"
-  /root/.acme.sh/acme.sh --install-cert -d "$DOMAIN_DUCKDNS" \
+  /root/.acme.sh/acme.sh \
+    --install-cert \
+    -d "$DOMAIN_DUCKDNS" \
+    -d "*.$DOMAIN_DUCKDNS" \
     --key-file "/certs/${DOMAIN_DUCKDNS}.key" \
     --fullchain-file "/certs/${DOMAIN_DUCKDNS}.crt" \
     --reloadcmd "echo [INFO] Certificado renovado para $DOMAIN_DUCKDNS"
